7868cd9e4ac0dbc6fa473bea0e695f1aa6205ae5
Alexander Diaz <aliensanderdiaz@gmail.com>
2023-07-05 07:22:08 -0500

    sesion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(''),
+
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
